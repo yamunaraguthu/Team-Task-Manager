8e7f678ae67ac39975bc9276aca8969c200e346d
Yamuna Raguthu <yamunaraguthu2003@gmail.com>
2026-05-08 18:32:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,29 +9,13 @@ const app = express();
 const projectRoutes = require("./routes/projectRoutes");
 const authMiddleware = require("./middleware/authMiddleware");
 
+
 // Middleware
-app.use(cors());
-app.use(express.json());
-
-// MongoDB Connectionconst express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
-require("dotenv").config();
-
-const app = express();
-
-const projectRoutes = require("./routes/projectRoutes");
-const authMiddleware = require("./middleware/authMiddleware");
-
-
-// CORS FIX
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
-
 
 app.use(express.json());
 
@@ -88,40 +72,4 @@ app.listen(PORT, () => {
 
     console.log(`Server running on port ${PORT}`);
 
-});
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
-
-// Auth Routes
-app.use("/api/auth", require("./routes/authRoutes"));
-
-// Project Routes
-app.use("/api/projects", projectRoutes);
-
-// Task Routes
-app.use("/api/tasks", require("./routes/taskRoutes"));
-
-// Home Route
-app.get("/", (req, res) => {
-    res.json({
-        message: "Backend Connected Successfully 🚀"
-    });
-});
-
-// Protected Route
-app.get("/protected", authMiddleware, (req, res) => {
-
-    res.json({
-        message: "Protected Route Accessed",
-        user: req.user
-    });
-
-});
-
-// Railway Port Fix
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
 });
