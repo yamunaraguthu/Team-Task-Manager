9542b50180096095ffb4db6f79086f7b8420ee6c
Yamuna Raguthu <yamunaraguthu2003@gmail.com>
2026-05-08 18:15:13 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,24 +3,19 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
 
     name: {
-        type: String,
-        required: true
+        type: String
     },
 
     email: {
-        type: String,
-        required: true,
-        unique: true
+        type: String
     },
 
     password: {
-        type: String,
-        required: true
+        type: String
     },
 
     role: {
         type: String,
-        enum: ["admin", "member"],
         default: "member"
     }
 
