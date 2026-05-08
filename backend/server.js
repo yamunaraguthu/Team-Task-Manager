0c632a334b9a60ad499c2a014907b79533de5ca6
Yamuna Raguthu <yamunaraguthu2003@gmail.com>
2026-05-08 18:20:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,35 +9,32 @@ const app = express();
 const projectRoutes = require("./routes/projectRoutes");
 const authMiddleware = require("./middleware/authMiddleware");
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
 
-
-// AUTH ROUTES
+// Auth Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 
-
-// PROJECT ROUTES
+// Project Routes
 app.use("/api/projects", projectRoutes);
 
-
-// TASK ROUTES
+// Task Routes
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-
-// HOME ROUTE
+// Home Route
 app.get("/", (req, res) => {
     res.json({
         message: "Backend Connected Successfully 🚀"
     });
 });
 
-
-// PROTECTED ROUTE
+// Protected Route
 app.get("/protected", authMiddleware, (req, res) => {
 
     res.json({
@@ -47,8 +44,8 @@ app.get("/protected", authMiddleware, (req, res) => {
 
 });
 
-
-const PORT = 5000;
+// Railway Port Fix
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
